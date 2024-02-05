2f983c180083eccab6ba2f934e6313cc3ccdb2bf
ABEL-pixel-nube <labelardo.477@gmail.com>
2024-02-05 16:51:47 -0500

    se agrega yml

---
+++ src/main/java/Resultados/ModuloResultado/ModuloResultadoApplication.java
@@ -0,0 +1,13 @@
+package Resultados.ModuloResultado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloResultadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloResultadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Resultados/ModuloResultado/ModuloResultadoApplicationTests.java
@@ -0,0 +1,13 @@
+package Resultados.ModuloResultado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloResultadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
