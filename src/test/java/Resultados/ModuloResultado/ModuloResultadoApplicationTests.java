400558cfc13b97880f817f944a6f25be0934a811
ABEL-pixel-nube <labelardo.477@gmail.com>
2024-02-05 17:16:00 -0500

    "agregamos dependencia"

--- src/test/java/Resultados/ModuloResultado/ModuloResultadoApplicationTests.java
+++ src/test/java/Resultados/ModuloResultado/ModuloResultadoApplicationTests.java
@@ -1,11 +1,13 @@
 package Resultados.ModuloResultado;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 
 
 @DataJpaTest
+@AutoConfigureTestDatabase
 class ModuloResultadoApplicationTests {
 
 	@Test
